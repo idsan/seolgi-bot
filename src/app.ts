54410e5598d089122ab1fab0b674b8c6f29d4e56
MYEONGGWANG BANG <idsan.net@hotmail.com>
2021-06-01 02:14:10 +0900

    이더마인 채굴상태를 보여주는 명령 추가

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,11 @@ import axios from 'axios';
 import mongoose from "mongoose";
 import bluebird from "bluebird";
 import { kimp } from './middleware/Cryptocurrency';
+import { Ethermine } from './middleware/Cryptocurrency/Ethermine'
 import 'dotenv/config';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN');
-
+const ethermine = new Ethermine();
 // Connect to MongoDB
 const mongoUrl: string = process.env["MONGODB_URI"]!;
 mongoose.Promise = bluebird;
@@ -22,5 +23,59 @@ bot.hears('설기야', (ctx) => ctx.reply(`멍멍!`));
 
 // Cryptocurrency 관련
 bot.command('kimp', async (ctx) => ctx.reply(await kimp()));
+
+// 채굴 상태 보기 (본인)
+bot.command('minerstats', async (ctx) => {
+    let result = await ethermine.getMinerStats(ctx.message.from.id);
+    ctx.reply(result);
+});
+
+
+// 채굴자 정보 보기 (본인)
+bot.command('minerinfo', async (ctx) => {
+    let miningUser = await ethermine.getMinerInfo(ctx.message.from.id);
+
+    if (miningUser === null) {
+        ctx.reply('등록 된 정보가 없습니다. 지갑주소를 등록해주세요.');
+        return;
+    }
+
+    ctx.reply(`
+${ ctx.message.from.first_name }님의 정보입니다.
+
+[ Symbol ]
+${ miningUser.symbol }
+
+[ WalletAddress ]
+${ miningUser.walletAddress }
+`);
+});
+
+// 채굴자 정보 등록 (본인)
+bot.command('setminerinfo', async (ctx) => {
+    let uid = ctx.message.from.id;
+    let tokens = ctx.message.text.split(' ');
+
+    if (tokens.length !== 3) {
+        ctx.reply('명령어를 올바르게 입력하세요.');
+        return;
+    }
+
+    let symbol = tokens[1].toUpperCase();
+    let walletAddress = tokens[2];
+
+    if (!(symbol === 'ETH' || symbol === 'ETC')) {
+        ctx.reply('지원하지 않는 코인입니다.');
+        return;
+    }
+
+    try {
+        await ethermine.setMinerInfo(uid, symbol, walletAddress);
+        ctx.reply('수정 성공');
+    } catch (err) {
+        console.error(err)
+        ctx.reply('수정 실패');
+    }
+});
 
 bot.launch();
---
+++ src/middleware/Cryptocurrency/Ethermine.ts
@@ -0,0 +1,151 @@
+import axios from 'axios';
+import { Miner } from '../../models/Miner'
+
+let numberWithCommas = (x: string) => x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+
+class MinerStats {
+    unconfirmed?: number;
+    estimatedDailyEarnings: number;
+    estimatedWeeklyEarnings: number;
+    estimatedMonthlyEarnings: number;
+
+    constructor(public unpaid: number, public coinsPerMin: number, public currentHashrate: number, public averageHashrate: number, public reportedHashrate: number, public validShares: number, public staleShares: number, public invalidShares: number) {
+        this.unpaid = unpaid * 1e-18;
+        this.coinsPerMin = coinsPerMin;
+        this.estimatedDailyEarnings = coinsPerMin * 60 * 24;
+        this.estimatedWeeklyEarnings = this.estimatedDailyEarnings * 7
+        this.estimatedMonthlyEarnings = this.estimatedDailyEarnings * 30
+        this.currentHashrate = currentHashrate * 1e-6;
+        this.averageHashrate = averageHashrate * 1e-6;
+        this.reportedHashrate = reportedHashrate * 1e-6;
+        this.validShares = validShares;
+        this.staleShares = staleShares;
+        this.invalidShares = invalidShares;
+    }
+
+    getAllSharesCount() {
+        return this.validShares + this.staleShares + this.invalidShares;
+    }
+
+    getValidSharesPercentage() {
+        return Math.floor(this.validShares / this.getAllSharesCount() * 100);
+    }
+
+    getStaleSharesPercentage() {
+        return Math.floor(this.staleShares / this.getAllSharesCount() * 100);
+    }
+
+    getInvalidSharesPercentage() {
+        return Math.floor(this.invalidShares / this.getAllSharesCount() * 100);
+    }
+}
+
+export class Ethermine {
+    async getMinerStats(userId: number) {
+        let miner = await Miner.findOne({ userId: userId });
+
+        if (miner === null) {
+            return 'no';
+        }
+        
+        let address = miner.walletAddress;
+        let symbol = miner.symbol;
+        if (!(address.length === 42 && address.slice(0, 2) === '0x')) {
+            return 'Invalid address';
+        }
+
+        let ethermineAPIURI: string;
+
+        if (symbol === 'ETH') {
+            ethermineAPIURI = `https://api.ethermine.org/miner/${ address.slice(2) }/currentStats`;
+        } else {
+            ethermineAPIURI = `https://api-etc.ethermine.org/miner/${ address.slice(2) }/currentStats`;
+        }
+
+        console.log(ethermineAPIURI);
+
+        let minerStats;
+        let upbitTicker;
+
+        try {
+            minerStats = await axios.get(ethermineAPIURI, {
+                timeout: 10000
+            });
+    
+            upbitTicker = await axios.get(`https://api.upbit.com/v1/ticker?markets=KRW-${ symbol }`, {
+                timeout: 10000
+            });
+        } catch (err) {
+            return err.message;
+        }
+
+        if (minerStats.data.status === 'ERROR') {
+            return minerStats.data.error;
+        }
+
+        const ms = new MinerStats(minerStats.data.data.unpaid, minerStats.data.data.coinsPerMin, minerStats.data.data.currentHashrate, minerStats.data.data.averageHashrate, minerStats.data.data.reportedHashrate, minerStats.data.data.validShares, minerStats.data.data.staleShares, minerStats.data.data.invalidShares);
+
+        if (minerStats.data.data.unconfirmed !== null) {
+            ms.unconfirmed = minerStats.data.data.unconfirmed * 1e-18;
+        }
+        const tradePrice = upbitTicker.data[0].trade_price;
+        // console.log(ms);
+
+        return `
+${ (ms.unconfirmed !== undefined) ? `
+○ Immature Balance
+${ (ms.unconfirmed)?.toFixed(5) } ${ symbol } / ${ numberWithCommas((ms.unconfirmed * tradePrice).toFixed(0)) } KRW` : ``}
+
+○ Unpaid Balance
+${ (ms.unpaid).toFixed(5) } ${ symbol } / ${ numberWithCommas((ms.unpaid * tradePrice).toFixed(0)) } KRW
+
+○ Estimated Earnings
+(Daily)
+${ (ms.estimatedDailyEarnings).toFixed(5) } ${ symbol } / ${ numberWithCommas((ms.estimatedDailyEarnings * tradePrice).toFixed(0)) } KRW
+(Weekly)
+${ (ms.estimatedWeeklyEarnings).toFixed(5) } ${ symbol } / ${ numberWithCommas((ms.estimatedWeeklyEarnings * tradePrice).toFixed(0)) } KRW
+(Monthly)
+${ (ms.estimatedMonthlyEarnings).toFixed(5) } ${ symbol } / ${ numberWithCommas((ms.estimatedMonthlyEarnings * tradePrice).toFixed(0)) } KRW
+
+○ Hashrate (MH/s)
+Current ${ (ms.currentHashrate).toFixed(1) }
+Average ${ (ms.averageHashrate).toFixed(1) }
+Reported ${ (ms.reportedHashrate).toFixed(1) }
+
+○ Shares
+Valid ${ ms.validShares } ${ (ms.getValidSharesPercentage() !== 0) ? `(${ ms.getValidSharesPercentage() } %)`: '' }
+Stale ${ ms.staleShares } ${ (ms.getStaleSharesPercentage() !== 0) ? `(${ ms.getStaleSharesPercentage() } %)`: '' }
+Invalid ${ ms.invalidShares } ${ (ms.getInvalidSharesPercentage() !== 0) ? `(${ ms.getInvalidSharesPercentage() } %)`: '' }
+`;
+    }
+
+    async getMinerInfo(uid: number) {
+        let miningUser = await Miner.findOne({ userId: uid });
+        return miningUser;
+    }
+
+    async setMinerInfo(uid: number, symbol: string, walletAddress: string) {
+        let miningUser = await Miner.findOne({ userId: uid });
+
+        // 등록 된 정보가 없으면 CREATE
+        if (miningUser === null) {
+            let miner = new Miner();
+    
+            miner.userId = uid;
+            miner.symbol = symbol;
+            miner.walletAddress = walletAddress;
+        
+            await miner.save();
+            console.log(miner);
+
+            return '등록 성공';
+        }
+    
+        // 등록 된 정보가 있으면 UPDATE
+        try {
+            await Miner.updateOne({ userId: miningUser.userId }, { $set: { symbol: symbol, walletAddress: walletAddress } });
+        } catch (err) {
+            throw err;
+        }
+    }
+}
