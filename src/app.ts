525abbb755262dfbb77f83495ab8dabd1e8dd892
MYEONGGWANG BANG <idsan.net@hotmail.com>
2021-06-07 23:43:13 +0900

    indent (space 4->2) 수정

--- src/app.ts
+++ src/app.ts
@@ -14,52 +14,54 @@ const kimp = new Kimp();
 const mongoUrl: string = process.env["MONGODB_URI"]!;
 mongoose.Promise = bluebird;
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(
-    () => { console.log('DB Connected!') },
-).catch(err => {
-    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
-    // process.exit();
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('DB Connected!')
+})
+.catch(err => {
+  console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+  // process.exit();
 });
 
 bot.hears('설기야', (ctx) => ctx.reply(`멍멍!!`));
 
 // Cryptocurrency 관련
 bot.command('kimp', async (ctx) => {
-    let chat = await ctx.reply('데이터를 불러오는 중입니다. 잠시만 기다려 주세요.');    
-    bot.telegram.editMessageText(chat.chat.id, chat.message_id, undefined, await kimp.getKimp());
+  let chat = await ctx.reply('데이터를 불러오는 중입니다. 잠시만 기다려 주세요.');    
+  bot.telegram.editMessageText(chat.chat.id, chat.message_id, undefined, await kimp.getKimp());
 });
 
 // 채굴 상태 보기 (본인)
 bot.command('minerstats', async (ctx) => {
-    let result = await ethermine.getMinerStats(ctx.message.from.id);
-    ctx.reply(result);
+  let result = await ethermine.getMinerStats(ctx.message.from.id);
+  ctx.reply(result);
 });
 
 // 채굴자 정보 등록 (본인)
 bot.command('setminerinfo', async (ctx) => {
-    let uid = ctx.message.from.id;
-    let tokens = ctx.message.text.split(' ');
+  let uid = ctx.message.from.id;
+  let tokens = ctx.message.text.split(' ');
 
-    if (tokens.length !== 3) {
-        ctx.reply('명령어를 올바르게 입력하세요.');
-        return;
-    }
+  if (tokens.length !== 3) {
+    ctx.reply('명령어를 올바르게 입력하세요.');
+    return;
+  }
 
-    let symbol = tokens[1].toUpperCase();
-    let walletAddress = tokens[2];
+  let symbol = tokens[1].toUpperCase();
+  let walletAddress = tokens[2];
 
-    if (!(symbol === 'ETH' || symbol === 'ETC')) {
-        ctx.reply('지원하지 않는 코인입니다.');
-        return;
-    }
+  if (!(symbol === 'ETH' || symbol === 'ETC')) {
+    ctx.reply('지원하지 않는 코인입니다.');
+    return;
+  }
 
-    try {
-        await ethermine.setMinerInfo(uid, symbol, walletAddress);
-        ctx.reply('수정 성공');
-    } catch (err) {
-        console.error(err)
-        ctx.reply('수정 실패');
-    }
+  try {
+    await ethermine.setMinerInfo(uid, symbol, walletAddress);
+    ctx.reply('수정 성공');
+  } catch (err) {
+    console.error(err)
+    ctx.reply('수정 실패');
+  }
 });
 
 bot.launch();
--- src/models/Miner.ts
+++ src/models/Miner.ts
@@ -1,21 +1,18 @@
 import mongoose from "mongoose";
 
 export type MinerDocument = mongoose.Document & {
-    symbol: string;
-    walletAddress: string;
-    userId: number;
+  symbol: string;
+  walletAddress: string;
+  userId: number;
 };
 
-const minerSchema = new mongoose.Schema<MinerDocument>(
-    {
-        symbol: String,
-        walletAddress: String,
-        userId: Number
-    },
-    {
-        timestamps: true,
-        versionKey: false
-    }
-);
+const minerSchema = new mongoose.Schema<MinerDocument>({
+  symbol: String,
+  walletAddress: String,
+  userId: Number
+}, {
+  timestamps: true,
+  versionKey: false
+});
 
 export const Miner = mongoose.model<MinerDocument>("Miner", minerSchema);
